dea59d122e39bb1d82ee34e4e22a05c6e9c04373
BnbSolutions <vaniocvanioc91@gmail.com>
2026-02-12 17:09:49 +0000

    dad

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Blog from "./pages/Blog";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter basename="/moldova-trails">
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/tours" element={<Tours />} />
